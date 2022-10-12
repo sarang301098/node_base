86f8d467fc1baeaef48643b797cebefb12d33dbc
sarang301098 <sarangp3010@gmail.com>
2022-10-12 09:11:40 -0400

    feat: changes

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,10 +5,10 @@ echo "Deleting old images"
 docker system prune -f 2>/dev/null; true
 
 echo "Updating Docker Image"
-docker login --username cloudpeerbits --password cloud@peerbits123
-docker pull cloudpeerbits/propane-bros-api:latest
+docker login --username sarangpatel --password cloud@sarang_3010
+docker pull sarangpatel/node-base-api:latest
 
-docker rm -f propane-bros-api 2>> /dev/null
-docker rmi cloudpeerbits/propane-bros-api:latest
+docker rm -f node-base-api 2>> /dev/null
+docker rmi sarangpatel/propane-bros-api:latest
 
-docker run -d --name propane-bros-api --restart always -p 3333:3333 cloudpeerbits/propane-bros-api:latest
+docker run -d --name node-base-api --restart always -p 3333:3333 sarangpatel/node-base-api:latest
